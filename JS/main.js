6c59cd64116d0c928d5d375f0759b6cf4423c795
Eugeny Zhiharev <jjuchara@gmail.com>
2019-11-14 17:47:46 +0300

    Подключили модалки через JS

--- JS/main.js
+++ JS/main.js
@@ -46,35 +46,76 @@ $(document).ready(function(){
     });
 });
 
+/*----------  Функции отключения скрола при активации модалки   ----------*/
+function disableScroll(){
+  $('html, body').addClass('stop-scrolling');
+  $('body').bind('touchmove', function(e){e.preventDefault()});
+}
 
+/*----------  Функции включения скрола при активации модалки   ----------*/
+function enableScroll(){
+  $('html, body').removeClass('stop-scrolling');
+  $('body').unbind('touchmove');
+}
+
+/*----------  Функция активации модалок  ----------*/
+function modalCallOn() {
+  $('.modal-call').fadeIn()
+  $('.modal-content').animate({
+    width: '300px',
+}, 400 );
+}
+function modalMoreOn() {
+  $('.modal-more').fadeIn()
+  $('.modal-content').animate({
+    width: '300px',
+}, 400 );
+}
+
+/*----------  Функция для деактивации модалок  ----------*/
+function modalCallOff() {
+  $('.modal-call').fadeOut()
+  $('.modal-content').animate({
+    width: '0',
+}, 400 );
+}
+function modalMoreOff() {
+  $('.modal-more').fadeOut()
+  $('.modal-content').animate({
+    width: '0',
+}, 400 );
+}
+
+/*----------  Активируем модалки  ----------*/
+$('.button_empty, .mob-btn').on('click', function(e){
+    e.preventDefault()
+    modalCallOn();
+    disableScroll();
+  });
+$('.button_fill').on('click', function(e){
+    e.preventDefault()
+    modalMoreOn();
+    disableScroll();
+  });
+
+  /*----------  Деактивируем модалки  ----------*/
+  $('.close-btn').on('click', function(e){
+    e.preventDefault()
+    modalCallOff();
+    modalMoreOff()
+    enableScroll();
+  });
+  
+  /*----------  Деактивация модалок при клике на оверлей  ----------*/
+  $('.modal-call, .modal-more').click(function(e){
+    if(e.target == this)
+        $(this).fadeOut(600, enableScroll(), modalCallOff(), modalMoreOff());
+  });
   
 
 
 
 
-// !Сделать чтобы при нажатии на ссылку меню сворачивалось и убирался класс
-
-// function disableScroll(){
-//   $('html, body').addClass('stop-scrolling');
-//   $('body').bind('touchmove', function(e){e.preventDefault()});
-// }
-
-// function enableScroll(){
-//   $('html, body').removeClass('stop-scrolling');
-//   $('body').unbind('touchmove');
-// }
-
- 
-
-// $(document).click(function (e){
-//   var div = $(".nav")
-//   $('.burger__btn').removeClass('burger__btn_active');
-//   if (!div.is(e.target)
-//     && div.has(e.target).length === 0) {
-//         div.slideUp();
-//   }
-
-// });
 
 // $(window).on('load', function () {
 //   $('.html-teg__item, .price-html-teg__item').animate({
@@ -84,23 +125,6 @@ $(document).ready(function(){
 //   }, 1500);
 // });
 
-// $('.button_empty').on('click', function(e){
-//   e.preventDefault
-//   $('.modal').addClass('modal_active', disableScroll);
-// });
-
-// $('.close-btn').on('click', function(){
-//   $(this).parents('.modal').removeClass('modal_active');
-// });
-
-// $('.modal').click(function(event){
-//   if(event.target == this)
-//       $(this).fadeOut(600, enableScroll);
-//       // $('.pop-up').animate({
-//       //     width: '0',
-//       //     height: '0'
-//       // }, 400 );
-// });
-
+// 
 
 });
