5a1799d774204b546f60acfa8a26490091199c63
Eugeny Zhiharev <jjuchara@gmail.com>
2019-12-01 12:12:03 +0300

    slider bugs fixed

--- JS/main.js
+++ JS/main.js
@@ -3,31 +3,26 @@ $(document).ready(function(){
   /*----------  Инициализация Swiper Slider  ----------*/
   var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: 'true'
+    },
+    navigation: {
+      nextEl: '.next',
+      prevEl: '.prev'
+    },
     breakpoints: {
       320: {
         slidesPerView: 1,
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'bullets',
-          clickable: 'true'
-        },
       },
       640: {
         slidesPerView: 2,
         spaceBetween: 20,
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'bullets',
-          clickable: 'true'
-        },
       },
       1025: {
         slidesPerView: 3,
         spaceBetween: 20,
-        navigation: {
-          nextEl: '.next',
-          prevEl: '.prev'
-        },
       }
     }
   });
